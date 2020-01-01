78f6fa501303e4e01ec727518ee12e3d6ca379dc
Yi-Hua Chiu <mychiux413@gmail.com>
2020-01-01 20:47:31 +0800

    python-typing worked

--- test.sh
+++ test.sh
@@ -7,4 +7,8 @@ go test
 
 cd $origin_dir
 create-multi-langs --from_csv data/valid_format.csv --to_file tests/python/generated.py && \
-pytest tests/python/generated_t.py
+pytest tests/python/generated_t.py && rm tests/python/generated.py
+
+
+create-multi-langs --from_csv data/valid_format.csv --to_file tests/python/generated_typing.py --py_typing && \
+pytest tests/python/generated_typing_t.py && rm tests/python/generated_typing.py
