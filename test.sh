9109e0dd713697076994d5a7fa13d0555f14a70b
Yi-Hua Chiu <mychiux413@gmail.com>
2020-01-01 19:37:02 +0800

    update sh

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
+origin_dir=$PWD
+
 pip install . && \
 create-multi-langs --from_csv data/valid_format.csv --to_file tests/go/generated.go && \
 cd tests/go && \
 go test
+
+cd $origin_dir
+create-multi-langs --from_csv data/valid_format.csv --to_file tests/python/generated.py && \
+pytest tests/python/generated_t.py
